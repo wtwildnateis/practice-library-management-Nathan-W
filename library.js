cf0e63bd85fc7010cb04f46c6a5db5e4a7fce6b7
Nate <wtwildnateis@gmail.com>
2025-05-18 18:00:22 -0500

    Assignment for LaunchCode

--- library.js
+++ library.js
@@ -40,7 +40,7 @@ class LibraryItem {
     constructor(title, id) {
         this.title = title;
         this.id = id;
-        this.isAvailable = isAvailable = true;
+        this.isAvailable = true;
     }
 
 
